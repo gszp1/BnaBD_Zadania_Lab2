dbb90e1929abeb91e554c11af0d4813ca0680774
gszp1 <grzybekmateusz@poczta.fm>
2024-04-13 00:18:24 +0200

    Adds methods for sorting and paginating.'

--- src/main/java/org/example/zadania_lab2/repository/EmployeeRepository.java
+++ src/main/java/org/example/zadania_lab2/repository/EmployeeRepository.java
@@ -1,7 +1,20 @@
 package org.example.zadania_lab2.repository;
 
 import org.example.zadania_lab2.model.Employee;
+
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.awt.print.Pageable;
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    List<Employee> findAllByOrderByIdAsc();
+
+    List<Employee> findAllByOrderByIdDesc();
+
+    Page<Employee> findAllByOrderByIdAsc(Pageable pageable);
+
+    Page<Employee> findAllByOrderByIdDesc(Pageable pageable);
 }
